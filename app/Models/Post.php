a72e86ee066ff43c3db9d009504a717e4a92b23e
Oliver Morris <blueman7700@gmail.com>
2021-10-27 17:51:44 +0100

    Added relationship functions to models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -22,6 +22,11 @@ class Post extends Model
 
     public function comments()
     {
-        return $this->hasMany('App\Comment');
+        return $this->hasMany(Comment::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
