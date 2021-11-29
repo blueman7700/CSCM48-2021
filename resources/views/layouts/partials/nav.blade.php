96e925116237274b62fcd2feb9b318b5e9834771
Oliver Morris <blueman7700@gmail.com>
2021-11-29 12:54:06 +0000

    added bootstrap dependencies

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -5,7 +5,7 @@
                 <h4 class="text-black">About</h4>
                     <p class="text-muted">Lorem Ipsum</p>
             </div>
-        
+
             <div class="col-sm-4 py-4">
                 <h4 class="text-black">Contact</h4>
                 <ul class="list-unstyled">
@@ -20,6 +20,8 @@
 <div class="navbar navbar-inverse bg-dark">
     <div class="container d-flex justify-content-between">
         <a href="#" class="navbar-brand">Users</a>
-            <button class="btn btn-dark" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">Navigation</button>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-expand-arrows-alt"></i>
+            </button>
     </div>
 </div>
